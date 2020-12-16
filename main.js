97d97018ec2acdf26f40420353db9fa3d7a388dd
miceyjuice <ganjafz@gmail.com>
2020-12-17 00:58:47 +0100

    Using 2 APIs to display current weather and the forecast

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const cityNames = !localStorage.getItem("cities")
   ? []
   : JSON.parse(localStorage.getItem("cities"));
 
-const timeStampToFullTime = (timeStamp) => {
-  const fullDate = new Date(timeStamp * 1000);
+const timeStampToFullTime = (timeStamp, timezone) => {
+  const fullDate = new Date((timeStamp + timezone) * 1000);
   const hour = fullDate.getHours();
   const minute = fullDate.getMinutes();
   const second = fullDate.getSeconds();
@@ -16,20 +16,22 @@ const getData = (url) =>
     .then((response) => response.json())
     .then((data) => data);
 
-const returnDataFromAPI = async (cityName) => {
-  const data = await getData(
+const returnWeatherForecastFromAPI = async (cityName) =>
+  await getData(
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
       cityName +
       "&appid=7d2502ee1821fc0c297b265ffbffa853&units=metric"
   );
-  console.log(data);
 
-  return data;
-};
+const returnCurrentWeatherFromAPI = async (cityName) =>
+  await getData(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=7d2502ee1821fc0c297b265ffbffa853&units=metric"
+  );
 
-const addToLocalStorage = (cityName, data) => {
+const addToLocalStorage = (cityName, data) =>
   localStorage.setItem(cityName, data);
-};
 
 // api.openweathermap.org/data/2.5/forecast?q=
 // &appid=7d2502ee1821fc0c297b265ffbffa853
@@ -43,7 +45,7 @@ const createWeatherBlock = (data) => {
     <div class="weatherBlock__topInfo">
     <div class="weatherBlock__topInfo__left">
         <div class="weatherBlock__topInfo__left__temperature">
-            <h3>${Math.round(data.list[0].main.temp)}°</h3>
+            <h3>${Math.round(data.current.temp)}°</h3>
         </div>
         <div class="weatherBlock__topInfo__left__city">
             <div class="weatherBlock__topInfo__left__city__img"></div>
@@ -52,7 +54,7 @@ const createWeatherBlock = (data) => {
         </div>
         <div class="weatherBlock__topInfo__right">
             <img class="weatherBlock__topInfo__right__img" src="./assets/${
-              data.list[0].weather[0].main
+              data.current.type
             }.png"></img>
         </div>
     </div>
@@ -64,19 +66,29 @@ const createWeatherBlock = (data) => {
         </div>
         <div class="weatherBlock__additionalInfo__value">
             <p class="weatherBlock__additionalInfo__value__text">
-              ${data.list[0].main.humidity}%
+              ${data.current.humidity}%
             </p>
         </div>
     </div>
     <div class="weatherBlock__hourlyForecast">
         <div class="weatherBlock__hourlyForecast__hour">
             <p class="weatherBlock__hourlyForecast__hour__text">
-              ${data.list[1].dt_txt}
+              ${data.list[0].dt_txt}
             </p>
         </div>
         <div class="weatherBlock__hourlyForecast__value">
             <p class="weatherBlock__hourlyForecast__value__text">
-                ${Math.round(data.list[1].main.temp)}°
+                ${Math.round(data.list[0].main.temp)}°
+            </p>
+        </div>
+        <div class="weatherBlock__hourlyForecast__hour">
+            <p class="weatherBlock__hourlyForecast__hour__text">
+            ${data.list[1].dt_txt}
+            </p>
+        </div>
+        <div class="weatherBlock__hourlyForecast__value">
+            <p class="weatherBlock__hourlyForecast__value__text">
+            ${Math.round(data.list[1].main.temp)}°
             </p>
         </div>
         <div class="weatherBlock__hourlyForecast__hour">
@@ -107,16 +119,6 @@ const createWeatherBlock = (data) => {
         <div class="weatherBlock__hourlyForecast__value">
             <p class="weatherBlock__hourlyForecast__value__text">
             ${Math.round(data.list[4].main.temp)}°
-            </p>
-        </div>
-        <div class="weatherBlock__hourlyForecast__hour">
-            <p class="weatherBlock__hourlyForecast__hour__text">
-            ${data.list[5].dt_txt}
-            </p>
-        </div>
-        <div class="weatherBlock__hourlyForecast__value">
-            <p class="weatherBlock__hourlyForecast__value__text">
-            ${Math.round(data.list[5].main.temp)}°
             </p>
         </div>
     </div>
@@ -154,10 +156,19 @@ const getDataFromInput = () => {
 
     if (cityInput == "") alert("Input can't be empty!");
     else {
-      let data = await returnDataFromAPI(cityInput);
-      console.log(data);
-      if (data.cod == 404) alert("There's no such city!");
-      addToLocalStorage(cityInput, JSON.stringify(data));
+      let currentWeather = await returnCurrentWeatherFromAPI(cityInput);
+      let weatherForecast = await returnWeatherForecastFromAPI(cityInput);
+      if (weatherForecast.cod == 404) alert("There's no such city!");
+      const fullWeatherForecast = weatherForecast;
+      fullWeatherForecast.current = {
+        temp: currentWeather.main.temp,
+        time: currentWeather.dt,
+        timezone: currentWeather.timezone,
+        type: currentWeather.weather[0].main,
+        humidity: currentWeather.main.humidity,
+      };
+      console.log(fullWeatherForecast);
+      addToLocalStorage(cityInput, JSON.stringify(fullWeatherForecast));
       location.reload();
     }
   });
